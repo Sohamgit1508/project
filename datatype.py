6bab3ae5d948e74ceb9182df0689aa7b07eda651
Sohamgit1508 <sohamsharma19812013>
2026-01-22 22:02:54 +0530

    message

---
+++ datatype.py
@@ -0,0 +1,8 @@
+a = 7
+print(type(a))
+b=2.6
+print(type(b))
+c="soham"
+print(type(c))
+name=input("enter your name:")
+print(type(name))
---
+++ project.py
@@ -0,0 +1,5 @@
+#input will allow user to enter anything
+name=input("enter your name:")
+print("hello,"+ name)
+birthday=input("when is your birthday:")
+print("your birthday is on"+ birthday)
