400c06975be87cf8434eccd4b4c1593bfa4d9d67
Sohamgit1508 <sohamsharma19812013>
2026-01-23 21:11:44 +0530

    message

---
+++ project2.py
@@ -0,0 +1,7 @@
+x=int(input("enter number of x:"))
+y=int(input("enter number of y:"))
+z=int(input("enter number of z:"))
+a=int(input("enter number of a :"))
+sum=x+y+z+a
+average=sum/4
+print(average)
