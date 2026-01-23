b8c0bc3ca62787643f3f15e919ed5bd21fcf457c
Sohamgit1508 <sohamsharma19812013>
2026-01-23 16:19:49 +0530

    message

--- project.py
+++ project.py
@@ -1,5 +1,35 @@
 #input will allow user to enter anything
 name=input("enter your name:")
 print("hello,"+ name)
-birthday=input("when is your birthday:")
-print("your birthday is on"+ birthday)
+birthday=int(input("when is your birthday:"))
+print("ohh! your birthday is on "+ birthday)
+
+#repeating the same process
+name2=input("enter your name:")
+print("hello,"+ name2)
+birthday2=int(input("when is your birthday:"))
+print("ohh! your birthday is on "+ birthday2)
+
+name3=input("enter your name:")
+print("hello,"+ name3)
+birthday3=int(input("when is your birthday:"))
+print("ohh! your birthday is on "+ birthday3)
+
+name4=input("enter your name:")
+print("hello,"+ name4)
+birthday4=int(input("when is your birthday:"))
+print("ohh! your birthday is on "+ birthday4)
+
+name5=input("enter your name:")
+print("hello,"+ name5)
+birthday5=int(input("when is your birthday:"))
+print("ohh! your birthday is on "+ birthday5)
+
+'''now to add one one line space between these two i can use print() and if i want to add two line space i can use print("\n")'''
+print() #this will add a line space
+#final output
+print(name+"'s birthday is on "+ birthday)
+print(name2+"'s birthday is on "+ birthday2)
+print(name3+"'s birthday is on "+ birthday3)
+print(name4+"'s birthday is on "+ birthday4)
+print(name5+"'s birthday is on "+ birthday5)
