27d708d03da5d0b6871b95790b41d4ef2c7e4333
Sohamgit1508 <sohamsharma19812013@gmail.com>
2026-01-23 17:00:02 +0530

    Change birthday input to accept string values
    Changed birthday input from integer to string to allow for non-numeric input.

--- project.py
+++ project.py
@@ -1,28 +1,28 @@
 #input will allow user to enter anything
 name=input("enter your name:")
 print("hello,"+ name)
-birthday=int(input("when is your birthday:"))
+birthday=input("when is your birthday:")
 print("ohh! your birthday is on "+ birthday)
 
 #repeating the same process
 name2=input("enter your name:")
 print("hello,"+ name2)
-birthday2=int(input("when is your birthday:"))
+birthday2=input("when is your birthday:")
 print("ohh! your birthday is on "+ birthday2)
 
 name3=input("enter your name:")
 print("hello,"+ name3)
-birthday3=int(input("when is your birthday:"))
+birthday3=input("when is your birthday:")
 print("ohh! your birthday is on "+ birthday3)
 
 name4=input("enter your name:")
 print("hello,"+ name4)
-birthday4=int(input("when is your birthday:"))
+birthday4=input("when is your birthday:")
 print("ohh! your birthday is on "+ birthday4)
 
 name5=input("enter your name:")
 print("hello,"+ name5)
-birthday5=int(input("when is your birthday:"))
+birthday5=input("when is your birthday:")
 print("ohh! your birthday is on "+ birthday5)
 
 '''now to add one one line space between these two i can use print() and if i want to add two line space i can use print("\n")'''
