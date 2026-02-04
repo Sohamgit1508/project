0488d1713ad89eaf497e2128ec5e991e377a5ed5
Sohamgit1508 <sohamsharma19812013>
2026-02-04 22:15:39 +0530

    message

---
+++ activity1lesson11.py
@@ -0,0 +1,12 @@
+x=int(input("enter the number:"))
+sum=0
+i=1
+while i<=x:
+    sum=sum+i
+    i=i+1
+print("the sum of first",x,"natural number is:",sum)
+
+i = 0
+while i <=0:
+    print("I WILL RUN FOREVER")
+    
