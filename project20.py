6ca6af60d63ec172a1d109a8e4015df883e63e0f
Sohamgit1508 <sohamsharma19812013>
2026-02-22 15:33:00 +0530

    message

---
+++ project20.py
@@ -0,0 +1,11 @@
+try:
+    age = input("Enter the age: ")
+    if age!=int: '''you can also use the in - built function called is.digit() to check if the input is a integer or  not.'''
+    raise ValueError("the age is not valid")
+    else:
+        print("The age is valid")
+finally:
+    if age%2=0
+      print("your age is even")
+    else:
+        print("the age is odd")
